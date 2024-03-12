6333a6903dd0e926714f29829f76241ccf1c7269
Cameron Berg <54913067+camberg23@users.noreply.github.com>
2024-03-12 13:42:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -301,9 +301,9 @@ elif st.session_state.page == 5:
 elif st.session_state.page == 6:
     st.balloons()
     with st.expander("**Comprehensive synthesis from responses**"):
-        st.write(st.session_state.insights)
+        st.html(st.session_state.insights)
     with st.expander("**Concrete job search recommendations**"):
-        st.write(st.session_state.recommendations)
+        st.html(st.session_state.recommendations)
     
     sendgrid_username = 'apikey'
     sendgrid_password = st.secrets['SENDGRID']
