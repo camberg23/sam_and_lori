375f22c1c4ed3a8c7d1a73b97f2190fe67649c0d
Cameron Berg <54913067+camberg23@users.noreply.github.com>
2024-10-31 23:11:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ else:
     if st.session_state.page == 1:  
         st.subheader("Wecome to Your Career Compass!")
         st.write("This tool will take 15 minutes to complete. Once you are finished, you will receive a career report custom-tailored to your personality, interests, and skills!")
-        st.write('**Note: this tool is a demo and has limitations. For this reason, it is best to complete the form in one sitting; if the app is abandoned for too long, you will lose your progress!** Please begin by filling in your basic information below:"')# Placeholder for more specific instructions
+        st.write('**Note: this tool is a demo and has limitations. For this reason, it is best to complete the form in one sitting; if the app is abandoned for too long, you will lose your progress!** Please begin by filling in your basic information below:')# Placeholder for more specific instructions
     
         # Placeholder for error messages
         error_placeholder = st.empty()
